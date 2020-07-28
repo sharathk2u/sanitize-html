c20d94a4edc1878eb8d2392196b22b6997b83f91
sharathk2u <sharath2u@gmail.com>
2020-07-28 19:37:40 +0530

    ignore empty attributes

--- src/index.js
+++ src/index.js
@@ -332,7 +332,7 @@ function sanitizeHtml(html, options, _recursing) {
               }
             }
             result += ' ' + a;
-            if (value.length) {
+            if (value != null && value != undefined) {
               result += '="' + escapeHtml(value, true) + '"';
             }
           } else {
